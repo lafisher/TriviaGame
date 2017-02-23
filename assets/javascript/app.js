3c474f130d6ff44bbc141426d74899f7803bd44c
Leigh Fisher <leighfisher@Jamess-MacBook-Air.local>
2017-02-22 22:24:55 -0600

    begin html css

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+//let's psuedo code this B
+//create global variable []s for questions, multiple choice, and correct answers
+//ensure arrays coralate with one another corectly  
+//create game form in html? append to? 
+//create timewrapper 
+//create if else for right answer, wrong answer, time out 
+//tally correct and incorect answers 
+//play music? 
+//display you win or you lose gifs or jpgs
+//reset button. my old enemy 
+var startGame;
+var questions =[];
+var choices =[];
+var answers =[];
+var correctAnswers = 0;
+var incorrectAnswers = 0;
+var noAnswer = 0;
+var timeWarp = 30;
+var chooseAnswer;
+var tickTock;
+var 

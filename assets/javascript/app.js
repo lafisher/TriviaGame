37a5107196486a16fe42778ee310892d1e998ebe
Leigh Fisher <leighfisher@Jamess-MacBook-Air-2.local>
2017-02-25 17:49:53 -0600

    it works sorta

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 
 //let's psuedo code this B
-//create global variable []s for questions, multiple choice, and correct answers
-//ensure arrays coralate with one another corectly  
-//append questions to html  
-//create timewrapper 
-//create if else for right answer, wrong answer, time out 
-//tally correct and incorect answers 
-//display "you win", "you lose", or timeout gifs or jpgs
-//play mthe timewarp at the end of the game 
+//create global variable []s for questions, multiple choice, and correct answers - check
+//ensure arrays coralate with one another corectly ????? 
+//append questions to html - one wil go! 
+//create timer - well i times something! 
+//create if else for right answer, wrong answer, time out ????
+//tally correct and incorect answers - maybe this works
+//display "you win", "you lose", or timeout jpgs
+//play the timewarp at the end of the game 
 //reset button. my old enemy 
 var startGame; 
 var questions =["Richard O'Brien, who wrote the musical on which the movie was based, portrayed what character in the film version?", "The movie marked the feature film debut of which one of these principal actors?", "What was the title of Richard O'Brien's not-quite-sequel movie from 1980 that placed new actors in the roles of Brad and Janet?", "Which character in the film is the first to be turned into a statue by the Medusa Transducer?", "Known as The Home of Happiness, name the town in which the motion picture is set.", "Which cast member's mouth lip-synchs along with Science Fiction Double Feature during the opening credits of the film?","In 1990, the movie was released on VHS in the United States for the very first time. What was the retail price of this videocassette?","Near the movie's beginning, what historic event can be heard on the radio (and seen in the newspaper headline) when Brad and Janet are in the car on the way to Dr. Scott's home?","What song appeared on the Original Motion Picture Soundtrack, but was edited out of the U.S. version of the film?","What was the MPAA letter rating given to The Rocky Horror Picture Show?"];
@@ -26,7 +26,7 @@ var timerNum;
 var audio = new Audio("assets/rhps-jump2lft.wav");
 
 //audio.play();
-//oh look a bootstrap button i don't entirely understand. but i changed it to DANGER and that's nice
+//oh look a bootstrap button. I changed it to DANGER. that's nice
 function initialize() {
 	startGame = "<p class='text-center main-button-container'><a class='btn btn-danger btn-lg start-button' href='#' role='button'>Damnit Janet</a></p>";
 	$(".theLab").html(startGame);
@@ -34,14 +34,16 @@ function initialize() {
 initialize();
 //on click event to start game play
 $("body").on("click", ".start-button", function(event){
+	lostInTime();
 	
-function letThereBeQuestions() {
-	gamePlay = "<p class='text-center'>" + questions[questionsLeft] + "</p><p class='first-answer answer'>a) " + choices[questionsLeft][0] + "</p><p class='answer'>b) " + choices[questionsLeft][1] + "</p><p class='answer'>c) " + choices[questionsLeft][2] + "</p><p class='answer'>d) " + choices[questionsLeft][3] + "</p>";
-	$(".theLab").html(gamePlay);
-}
 letThereBeQuestions();
 
 });
+
+function letThereBeQuestions() {
+	gamePlay = "<p class='text-center'>" + questions[questionsLeft] + "</p><p class='first-answer answer'>a) " + choices[questionsLeft][0] + "</p><p class='answer'>b) " + choices[questionsLeft][1] + "</p><p class='answer'>c) " + choices[questionsLeft][2] + "</p><p class='answer'>d) " + choices[questionsLeft][3] + "</p>";
+	$(".theLab").prepend(gamePlay);
+}
 //on click event to choose answer 
 //note to self - come up with a better naming system answer and answers not so much
 $("body").on("click", ".answer", function(event){
@@ -62,23 +64,27 @@ $("body").on("click", ".answer", function(event){
 //recognize correct answer
 function generateWin() {
 	correctAnswers++;
-	gamePlay = "<p class='text-center'> Yes! Time for the floor show!" + answers[questionsLeft] + "</p>" ;
+	gamePlay = "<p class='text-center'> Yes! Time for the floor show! "  + answers[questionsLeft] + " is correct!" + "</p>";
 	$(".theLab").html(gamePlay);
-	//setTimeout();  
+	setTimeout(goTrivia, 4000);  
 };
 
 //recognize incorrect answer 
 function generateLoss() {
 	incorrectAnswers++;
-	gamePlay =  "<p class='text-center'> Oh Rocky! Frankie must have half your brain" + answers[questionsLeft] + "</p>" ;
+	gamePlay =  "<p class='text-center'> Oh Rocky! Frankie must have half your brain. The Answer is " + answers[questionsLeft] + "</p>" ;
 	$(".theLab").html(gamePlay);
-	//setTimeout(); 
+	setTimeout(goTrivia, 4000); 
 };
 
+function generateLossDueToTimeOut() {
+	noAnswer++;
+	gamePlay = "<p class='text-center'> ak! You've been Medusa Transducered! The Answer is " + answers[questionsLeft] + "</p>" ;
+	$(".theLab").html(gamePlay);
+	setTimeout(goTrivia, 4000);
+};
 //timer function 
-//problem- countdown is inside, other functions can't see. tried to seperate. broke the world
 function lostInTime() {
-
 //set countdown to starting time
 	timeLeft = 25;
 //set interval to create countdown 
@@ -86,23 +92,33 @@ function lostInTime() {
 
 	$(".timer").html("<div id='finalCountDown'><h5>Time Warp in . . .  " + timeLeft + "</h5></div>");
  	//countdown(); 
-  
-
 };
 
-			function countdown() {
+function countdown() {
 		if (timeLeft < 0) {
 			clearInterval(timerNum);
-			$(".theLab").append( "ak! You've been Medusa Transducered!");		
-			 audio.play();
+			//generateLossDueToTimeOut();					 
 		}
 		else {
 			$(".timer").html("<div id='finalCountDown'><h5>Time Warp in  . . . " + timeLeft + "</h5></div>");
 			timeLeft--;
 		}
+
 	};
 
-lostInTime(); // and lost in space. and meaning. 
+function goTrivia() {
+	if (questionsLeft < 9) {
+	questionsLeft++;
+	timeLeft = 25,
+	letThereBeQuestions();
+	countdown();
+	}
+	else {
+		audio.play();
+	}
+};
+
+ // and lost in space. and meaning. 
 })
 
 
